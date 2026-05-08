830076998fb9fe021f7d7c52b23d61cf015c26ff
Nguyen Huynh Duc <duc2.nh@vdsc.com.vn>
2026-05-08 17:41:48 +0700

    Refactor tube initialization and spacing; improve readability and maintainability

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ from random import randint
 pygame.init()
 
 # Tạo window
-screen = pygame.display.set_mode((400, 600))
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 400
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
 
 # Detect game còn chạy hay không
 running = True
+
+
+# Tạo FPS
+clock = pygame.time.Clock() 
+FPS = 60
 
 # Colors
 GREEN = (0, 200, 0)
@@ -20,15 +27,16 @@ BLUE = (0, 0, 255)
 TUBE_WIDTH = 50
 TUBE_HEIGHT_MIN = 100
 TUBE_HEIGHT_MAX = 400
-TUBE_SPACING = 150
+TUBE_SPACE_VERTICAL = 150
+TUBE_SPACE_HORIZONTAL = 150
 TUBE_VELOCITY = 3
 
 # TUBE Coordinate
 class Tube:
-    def __init__(self, tube_x: int, tube_y: int):
+    def __init__(self, tube_x: int, tube_y: int, tube_height: int | None = None):
         self.tube_x = tube_x
-        self.tube_y = tube_y
-        self.tube_height = randint(TUBE_HEIGHT_MIN, TUBE_HEIGHT_MAX)
+        self.tube_height = randint(TUBE_HEIGHT_MIN, TUBE_HEIGHT_MAX) if tube_height is None else tube_height
+        self.tube_y = tube_y if tube_y >= 0 else SCREEN_HEIGHT - self.tube_height
     
     def getRect(self) -> Tuple[Union[float, int], Union[float, int], Union[float, int], Union[float, int]]:
         return (self.tube_x, self.tube_y, TUBE_WIDTH, self.tube_height)
@@ -36,16 +44,14 @@ class Tube:
     def calcVelocity(self) -> None:
         self.tube_x -= TUBE_VELOCITY
 
-# Tạo FPS
-clock = pygame.time.Clock() 
-FPS = 60
-
 # Tạo Tubes
 tube_1_top = Tube(200, 0)
-tube_2_top = Tube(TUBE_WIDTH + TUBE_SPACING + tube_1_top.tube_x, 0)
-tube_3_top = Tube(TUBE_WIDTH + TUBE_SPACING + tube_2_top.tube_x, 0)
+tube_2_top = Tube(TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_1_top.tube_x, 0)
+tube_3_top = Tube(TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_2_top.tube_x, 0)
 
-tube_1_bot = Tube(200, screen.get_height())
+tube_1_bot = Tube(200, -1, SCREEN_HEIGHT - TUBE_SPACE_VERTICAL - tube_1_top.tube_height)
+tube_2_bot = Tube(TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_1_bot.tube_x, -1, SCREEN_HEIGHT - TUBE_SPACE_VERTICAL - tube_2_top.tube_height)
+tube_3_bot = Tube(TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_2_bot.tube_x, -1, SCREEN_HEIGHT - TUBE_SPACE_VERTICAL - tube_3_top.tube_height)
 
 while running:
     clock.tick(FPS) # Draw 60 frame trên 1 giây
@@ -56,20 +62,43 @@ while running:
     pygame.draw.rect(screen, BLUE, tube_3_top.getRect())
     
     pygame.draw.rect(screen, BLUE, tube_1_bot.getRect())
+    pygame.draw.rect(screen, BLUE, tube_2_bot.getRect())
+    pygame.draw.rect(screen, BLUE, tube_3_bot.getRect())
     
     tube_1_top.calcVelocity()
     tube_2_top.calcVelocity()
     tube_3_top.calcVelocity()
     
+    tube_1_bot.calcVelocity()
+    tube_2_bot.calcVelocity()
+    tube_3_bot.calcVelocity()
+    
     if tube_1_top.tube_x <= -TUBE_WIDTH:
-        tube_1_top.tube_x = TUBE_WIDTH + TUBE_SPACING + tube_3_top.tube_x
+        tube_1_top.tube_x = TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_3_top.tube_x
         tube_1_top.tube_height = randint(TUBE_HEIGHT_MIN, TUBE_HEIGHT_MAX)
+    
     if tube_2_top.tube_x <= -TUBE_WIDTH:
-        tube_2_top.tube_x = TUBE_WIDTH + TUBE_SPACING + tube_1_top.tube_x
+        tube_2_top.tube_x = TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_1_top.tube_x
         tube_2_top.tube_height = randint(TUBE_HEIGHT_MIN, TUBE_HEIGHT_MAX)
+    
     if tube_3_top.tube_x <= -TUBE_WIDTH:
-        tube_3_top.tube_x = TUBE_WIDTH + TUBE_SPACING + tube_2_top.tube_x
+        tube_3_top.tube_x = TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_2_top.tube_x
         tube_3_top.tube_height = randint(TUBE_HEIGHT_MIN, TUBE_HEIGHT_MAX)
+    
+    if tube_1_bot.tube_x <= -TUBE_WIDTH:
+        tube_1_bot.tube_x = TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_3_bot.tube_x
+        tube_1_bot.tube_height = SCREEN_HEIGHT - TUBE_SPACE_VERTICAL - tube_1_top.tube_height
+        tube_1_bot.tube_y = SCREEN_HEIGHT - tube_1_bot.tube_height
+        
+    if tube_2_bot.tube_x <= -TUBE_WIDTH:
+        tube_2_bot.tube_x = TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_1_bot.tube_x
+        tube_2_bot.tube_height = SCREEN_HEIGHT - TUBE_SPACE_VERTICAL - tube_2_top.tube_height
+        tube_2_bot.tube_y = SCREEN_HEIGHT - tube_2_bot.tube_height
+        
+    if tube_3_bot.tube_x <= -TUBE_WIDTH:
+        tube_3_bot.tube_x = TUBE_WIDTH + TUBE_SPACE_HORIZONTAL + tube_2_bot.tube_x
+        tube_3_bot.tube_height = SCREEN_HEIGHT - TUBE_SPACE_VERTICAL - tube_3_top.tube_height
+        tube_3_bot.tube_y = SCREEN_HEIGHT - tube_3_bot.tube_height
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
